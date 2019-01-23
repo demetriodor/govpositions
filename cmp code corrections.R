0aae165dc7c343bdc14e82253a098e3729dadece
demetriodor <33958042+demetriodor@users.noreply.github.com>
2019-01-23 10:51:29 +0100

    Create cmp code corrections.R
    This file imputes cmp (Manifesto project) party codes to the ParlGov parties dataset.

---
+++ cmp code corrections.R
@@ -0,0 +1,57 @@
+#CMP code assignment
+parties[parties$country_name=="Austria" & parties$party_name_english=="Team Stronach",'cmp2']<- 42951
+parties[parties$country_name=="Bulgaria" & parties$party_name_english=="Bulgaria Without Censorship",'cmp2']<- 80630
+parties[parties$country_name=="Bulgaria" & parties$party_name_english=="Patriotic Union",'cmp2']<- 80062
+parties[parties$country_name=="Bulgaria" & parties$party_name_english=="Reformist Bloc",'cmp2']<- 80061
+parties[parties$country_name== "Bulgaria" & parties$party_name_english=="Order, Lawfulness and Justice",'cmp2']<- 80620
+parties[parties$country_name=="Cyprus"  & parties$party_name_english=="Free Citizens Movement",'cmp2']<-55340
+parties[parties$country_name=="Czechia"  & parties$party_name_english=="Freedom and Direct Democracy Tomio Okamura" ,'cmp2']<-82721
+parties[parties$country_name=="Czechia"  & parties$party_name_english=="Dawn of Direct Democracy" ,'cmp2']<-82720
+parties[parties$country_name=="Germany" & parties$party_name_english=="German Pirate Party",'cmp2']<-41952
+parties[parties$country_name=="Spain"  & parties$party_name_english=="Yes" ,'cmp2']<-33091
+parties[parties$country_name=="Spain"  & parties$party_name_english=="Compromise | A la valenciana" ,'cmp2']<-33093
+parties[parties$country_name=="Spain"  & parties$party_name_english=="We Can" ,'cmp2']<-33025
+parties[parties$country_name=="Estonia"  & parties$party_name_english=="Estonian Rural Centre Party" ,'cmp2']<-83810
+parties[parties$country_name=="France"  & parties$party_name_english=="Unbowed France",'cmp2']<-31240
+parties[parties$country_name=="France"  & parties$party_name_english=="The Republic Onwards!" ,'cmp2']<-31425
+parties[parties$country_name=="Croatia"  & parties$party_name_english=="Croatian Democratic Alliance of Slavonia and Baranja",'cmp2']<- 81956
+parties[parties$country_name=="Croatia"  & parties$party_name_english=="Independent List -- PhD Ivan Grubisic" ,'cmp2']<-81440
+parties[parties$country_name=="Croatia"  & parties$party_name_english=="Milan Bandic 365 -- The Party of Labour and Solidarity",'cmp2']<- 81961
+parties[parties$country_name=="Croatia"  & parties$party_name_english=="People's Party -- Reformists",'cmp2']<- 81450
+parties[parties$country_name=="Croatia"  & parties$party_name_english=="Independent Democratic Serb Party" ,'cmp2']<-81951
+parties[parties$country_name=="Hungary"  & parties$party_name_english=="Together -- Party for a New Era" ,'cmp2']<-86340
+parties[parties$country_name=="Iceland"  & parties$party_name_english=="Civic Movement -- The Movement",'cmp2']<- 15430
+parties[parties$country_name=="Italy"  & parties$party_name_english=="Brothers of Italy -- National Centre-right",'cmp2']<- 32630
+parties[parties$country_name=="Italy" & parties$party_name_english=="Popular Party for Prodi",'cmp2']<-32955
+parties[parties$country_name=="Italy"  & parties$party_name_english=="Left (Ecology) Freedom",'cmp2']<- 32230
+parties[parties$country_name=="Lithuania"  & parties$party_name_english=="Order and Justice -- Liberal Democratic Party",'cmp2']<- 88041
+parties[parties$country_name=="Latvia"  & parties$party_name_english=="Latvian First Party / Latvian Way Party",'cmp2']<- 87041
+parties[parties$country_name=="Netherlands" & parties$party_name_english== "General Senior Union | United Seniors Party" ,'cmp2']<-22954
+parties[parties$country_name=="Netherlands"  & parties$party_name_english=="Think" ,'cmp2']<-22321
+parties[parties$country_name=="Poland"  & parties$party_name_english=="Your (Palikot's) Movement",'cmp2']<- 92440
+parties[parties$country_name=="Portugal" & parties$party_name_english=="Party for Animals and Nature",'cmp2']<- 35120
+parties[parties$country_name== "Romania" & parties$party_name_english=="People's Party -- Dan Diaconescu" ,'cmp2']<-93981
+parties[parties$country_name=="Romania" & parties$party_name_english== "National Union for the Progress of Romania",'cmp2']<- 93001
+parties[parties$country_name=="Slovakia" & parties$party_name_english=="We are family -- Boris Kollar" ,'cmp2']<-96725
+parties[parties$country_name=="Slovakia" & parties$party_name_english=="People's Party Our Slovakia" ,'cmp2']<-96720
+parties[parties$country_name== "Slovenia" & parties$party_name_english=="Civic List" ,'cmp2']<-97450
+parties[parties$country_name=="Slovenia"  & parties$party_name_english=="Zoran Jankovic's List -- Positive Slovenia",'cmp2']<- 97340
+parties[parties$party_name_english=='Citizens for European Development of Bulgaria', 'cmp2']<-80510 
+parties[parties$country_name=="Slovenia"  & parties$party_id==179,'cmp2']<- 97320
+parties[parties$country_name=="Slovenia"  & parties$party_id==706,'cmp2']<- 97322
+parties[parties$country_name=="Slovakia"  & parties$party_id==1620,'cmp2']<- 96955
+parties[parties$country_name=="Latvia"  & parties$party_id==445,'cmp2']<- 87723
+parties[parties$country_name=="Latvia"  & parties$party_id==1942,'cmp2']<- 87620
+parties[parties$country_name=="Latvia"  & parties$party_id==521,'cmp2']<- 87071
+parties[parties$country_name=="Finland"  & parties$party_id==1292,'cmp2']<- 14223
+parties[parties$country_name=="Portugal"  & parties$party_id==251,'cmp2']<- 35520
+parties[parties$country_name=="Lithuania"  & parties$party_id==1421,'cmp2']<- 88460
+parties[parties$country_name=="Italy"  & parties$party_id==226,'cmp2']<- 32530
+parties[parties$country_name=="Belgium"  & parties$party_id==915,'cmp2']<- 21426
+parties[parties$country_name=="Bulgaria"  & parties$party_id==2211,'cmp2']<- 80071
+parties[parties$country_name=="Belgium"  & parties$party_id==454,'cmp2']<- 21425
+parties[parties$country_name=="Belgium"  & parties$party_id==1113,'cmp2']<- 21221
+parties[parties$country_name=="Poland"  & parties$party_id==838,'cmp2']<- 92212
+parties[parties$country_name=="Poland"  & parties$party_id==629,'cmp2']<- 92212
+parties[parties$country_name=="Austria"  & parties$party_id==1536,'cmp2']<- 42710
+parties[parties$country_name=="Romania"  & parties$party_id==1120,'cmp2']<- 93223
